b99202bd999d84ae31bf259fe578ba4d401d153d
daffy weng <daffy.weng@mangatoon.mobi>
2021-01-07 22:34:16 +0800

    1.0

---
+++ src/AsyncEventDispatchJob.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MangaToon server projects.
+ */
+namespace Hyperf\AsyncEvent;
+
+use Hyperf\AsyncQueue\Job;
+use Hyperf\Utils\ApplicationContext;
+
+class AsyncEventDispatchJob extends Job
+{
+    public $event;
+
+    protected $maxAttempts = 0;
+
+    public function __construct($event)
+    {
+        $this->event = $event;
+    }
+
+    public function handle()
+    {
+        ApplicationContext::getContainer()->get(AsyncEventDispatcher::class)->dispatchNow($this->event);
+    }
+}
---
+++ src/AsyncEventDispatchJobService.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MangaToon server projects.
+ */
+namespace Hyperf\AsyncEvent;
+
+use Hyperf\AsyncQueue\Driver\DriverFactory;
+use Hyperf\AsyncQueue\Driver\DriverInterface;
+
+class AsyncEventDispatchJobService
+{
+    /**
+     * @var DriverInterface
+     */
+    protected $driver;
+
+    public function __construct(DriverFactory $driverFactory)
+    {
+        $this->driver = $driverFactory->get('async_event');
+    }
+
+    /**
+     * @param $object
+     */
+    public function push(object $object, int $delay = 0): bool
+    {
+        $this->driver->push($object, $delay);
+        return true;
+    }
+}
